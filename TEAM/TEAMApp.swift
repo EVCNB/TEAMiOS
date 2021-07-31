d46b618eb36553f85013f8b20ce2b5db01fd4244
Mark Adamcin <adamcin@gmail.com>
2021-07-31 10:48:36 -0700

    Initial Commit

---
+++ TEAM/TEAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEAMApp.swift
+//  TEAM
+//
+//  Created by Mark Adamcin on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct TEAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
