74f462e0a45b9abdd9b9aa31e9f28929dd3c9a12
Mark Adamcin <adamcin@users.noreply.github.com>
2021-08-15 21:00:48 +0000

    finish upload-ipa.sh script

--- upload-ipa.sh
+++ upload-ipa.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 # https://rderik.com/blog/automating-build-and-testflight-upload-for-simple-ios-apps/#automating-the-build-version-increase
+set -eo pipefail
+readonly basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "${basedir}"
+
+app_version="$(plutil -extract CFBundleShortVersionString xml1 -o - TEAM/Info.plist | xmllint --xpath "//string/text()" -)"
+
+current_project_version=0
+if git config -f versions.gitconfig --get-regexp "team.v${app_version}.b" >>/dev/null; then
+    current_project_version="$(git config -f versions.gitconfig --get-regexp "team.v${app_version}.b" | cut -f2 -db | cut -f1 -d' ' | sort -rn | head -n 1)"
+fi
+
+new_project_version=$((current_project_version + 1))
+
+CURRENT_PROJECT_VERSION="${new_project_version}" xcodebuild -project TEAM.xcodeproj -scheme TEAM -sdk iphoneos -configuration Release archive -archivePath "$(pwd)/build/TEAM.xcarchive"
+
+xcodebuild -exportArchive -archivePath "$(pwd)/build/TEAM.xcarchive" -exportOptionsPlist exportOptions.plist -exportPath "$(pwd)/build"
+
 git config -f versions.gitconfig "team.v${app_version}.b${new_project_version}" "$(git rev-parse --short HEAD)"
-git config -f versions.gitconfig --get-regexp team.v1.0.b | cut -f2 -db | cut -f1 -d' ' | sort -rn | head -n 1
+
+git add versions.gitconfig
